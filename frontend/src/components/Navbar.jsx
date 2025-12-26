988ddbb80a8c45de706c41ca922b48ce2ab98f50
Sowrav Chandra Das <sowravdas003@gmail.com>
2025-12-26 19:08:30 +0600

    frontend start

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+    <nav>
+        <h1 className='bg-blue-100'>Navbar</h1>
+    </nav>
+    </>
+  )
+}
+
+export default Navbar
